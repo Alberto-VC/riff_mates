c5c1b4e2e34eeead05bae2989ef16f921618e7c1
Alberto-VC <37180792@uaz.edu.mx>
2025-02-15 14:12:02 -0600

    home, about and version apps added.

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+
+# Create your views here.
+
+def credits(request):
+    content = "Nicky\nAlberto"
+
+    return HttpResponse(content, content_type="text/plain")
+
